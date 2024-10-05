82ffe5c9dd2ddfe41b8104679a7b4c62e7a07a43
MMM1381 <mehdimoazedy@gmail.com>
2024-10-05 10:03:14 +0330

    Update Mid_point_Algorithm.py
    each octant is defined

--- Mid_point_Algorithm.py
+++ Mid_point_Algorithm.py
@@ -4,74 +4,45 @@
 # if F(p)<0, the point is inside the circle
 # F(p)=0, the point is on the perimeter
 # F(p)>0, the point is outside the circle
-#  
+
+
 def midPointCircleDraw(x_centre, y_centre, r):
 	x = r
 	y = 0
-	
-	# Printing the initial point the 
-	# axes after translation 
-	print("(", x + x_centre, ", ", 
-			y + y_centre, ")", 
-			sep = "", end = "") 
-	
-	# When radius is zero only a single 
-	# point be printed 
-	if (r > 0) :
-	
-		print("(", x + x_centre, ", ",
-				-y + y_centre, ")", 
-				sep = "", end = "") 
-		print("(", y + x_centre, ", ", 
-				x + y_centre, ")",
-				sep = "", end = "") 
-		print("(", -y + x_centre, ", ", 
-					x + y_centre, ")", sep = "") 
-	
+	print(f"initial points: x={x+x_centre}, y={y+y_centre}")
 	# Initialising the value of P 
 	P = 1 - r 
 
 	while x > y:
-	
+		#by given pixel (x, y), the next pixel to be plotted is either (x, y+1) or (x-1, y+1).
 		y += 1
 		
 		# Mid-point inside or on the perimeter
 		if P <= 0: 
-			P = P + 2 * y + 1
-			
+			P = P + 2*y + 1
 		# Mid-point outside the perimeter 
 		else:		 
 			x -= 1
-			P = P + 2 * y - 2 * x + 1
-		
-		# All the perimeter points have 
-		# already been printed 
+			P = P + 2*y - 2*x + 1
+
+
+		# check if is finished
 		if (x < y):
 			break
 		
-		# Printing the generated point its reflection 
-		# in the other octants after translation 
-		print("(", x + x_centre, ", ", y + y_centre,
-							")", sep = "", end = "") 
-		print("(", -x + x_centre, ", ", y + y_centre, 
-							")", sep = "", end = "") 
-		print("(", x + x_centre, ", ", -y + y_centre,
-							")", sep = "", end = "") 
-		print("(", -x + x_centre, ", ", -y + y_centre,
-										")", sep = "") 
+		print(f"first octant: x={x+x_centre}, y={y+y_centre}")
+		print(f"fourth octant: x={-x+x_centre}, y={y+y_centre}")
+		print(f"eighth octant: x={x+x_centre}, y={-y+y_centre}")
+		print(f"fifth octant: x={-x+x_centre}, y={-y+y_centre}")
+
 		
-		# If the generated point on the line x = y then 
-		# the perimeter points have already been printed 
+		# If x==y we have reached the end point of our octant 
 		if x != y:
-		
-			print("(", y + x_centre, ", ", x + y_centre, 
-								")", sep = "", end = "") 
-			print("(", -y + x_centre, ", ", x + y_centre,
-								")", sep = "", end = "") 
-			print("(", y + x_centre, ", ", -x + y_centre,
-								")", sep = "", end = "") 
-			print("(", -y + x_centre, ", ", -x + y_centre, 
-											")", sep = "")
+			print(f"second octant: x={y+x_centre}, y={x+y_centre}")
+			print(f"third octant: x={-y+x_centre}, y={x+y_centre}")
+			print(f"seventh octant: x={y+x_centre}, y={-x+y_centre}")
+			print(f"sixth octant: x={-y+x_centre}, y={-x+y_centre}")
+
 							
 
 # To draw a circle of radius 3 
