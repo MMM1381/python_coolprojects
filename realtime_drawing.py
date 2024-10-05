47d49997768b7ba16b2799423ce573837bae4bb5
MMM1381 <mehdimoazedy@gmail.com>
2024-10-05 09:46:23 +0330

    Update realtime_drawing.py

--- realtime_drawing.py
+++ realtime_drawing.py
@@ -5,21 +5,19 @@ import yfinance as yf
 
 data = yf.download(tickers='BTC-USD', period='max', interval='1d')
 
-print(data)
-# data = pd.read_csv("Load06_500mN cut.csv")
-# count = 0
-# x = []
-# y = []
+count = 0
+x = []
+y = []
 
 
-# def draw_graph(i):
-#     global count
-#     count += 1
-#     x.append(data['Displacement'][count])
-#     y.append(data['Load'][count])
+def draw_graph(i):
+    global count
+    count += 1
+    x.append(count)
+    y.append(data['Close'].iloc[count])
 
-#     plt.cla()
-#     plt.plot(x,y)
+    plt.cla()
+    plt.plot(x,y)
 
-# anima = animation.FuncAnimation(plt.gcf(), draw_graph, interval=1500)
-# plt.show()
+anima = animation.FuncAnimation(plt.gcf(), draw_graph, interval=1500)
+plt.show()
