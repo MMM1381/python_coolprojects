852733abd409f01118c7c0fad68c73af4ce294c3
MohammadMehdi Moazedi <mehdimoazedy@gmail.com>
2024-11-24 08:29:00 +0330

    add date in index

--- realtime_drawing.py
+++ realtime_drawing.py
@@ -8,14 +8,14 @@ data = yf.download(tickers='BTC-USD', period='max', interval='1d')
 x = []
 y = []
 
-print(data)
+# print(data.columns)
 def draw_graph(i):
-    global count
-    x.append(i)
+    # print(data.index[i])
+    x.append(data.index[i])
     y.append(data['Close'].iloc[i])
 
     plt.cla()
     plt.plot(x,y)
 
-anima = animation.FuncAnimation(plt.gcf(), draw_graph,frames=len(data) ,interval=1500)
+anima = animation.FuncAnimation(plt.gcf(), draw_graph,frames=len(data) ,interval=1000)
 plt.show()
